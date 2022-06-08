14e738064f1085ee4f95a943cf8d20f5ca25302e
ksenia-kh <ksenia.kharitonova@estudiantat.upc.edu>
2022-06-08 10:53:13 +0200

    excl.logs, correct data loc.

--- finetuning_amd.py
+++ finetuning_amd.py
@@ -1,5 +1,5 @@
 from datasets import load_from_disk
-from transformers import AutoTokenizer, AutoModel, DataCollatorForSeq2Seq
+from transformers import AutoTokenizer, DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 CHECKPOINT = "/gpfs/projects/bsc88/huggingface/models/m2m100_418M"
@@ -19,27 +19,30 @@ tokenized_dataset = load_from_disk(DATASET_LOC)
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, padding=True)
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./results",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    save_total_limit=1,
-    num_train_epochs=1,
-    fp16=False,
-    save_strategy="epoch",
-    logging_strategy="epoch",
-)
+        output_dir="./results/epoch-1",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        weight_decay=0.01,
+        save_total_limit=1,
+        num_train_epochs=1,
+        fp16=False,
+        load_best_model_at_end=True,
+        save_strategy="steps",
+        save_steps=20000,
+        evaluation_strategy="steps",
+        eval_steps=10000,
+        logging_strategy="steps",
+     )
 
 trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["valid"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-)
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["valid"],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
 
 print("Starting training...")
 
