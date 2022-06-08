1e86867c6d9c621c474e85b9c829282edcd043c3
carlosep93 <carlos.e.p93@gmail.com>
2022-06-08 11:38:35 +0200

    finetuning amd adapters

---
+++ ft-amd-1gpu-adapter.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=ft_m2m
+#SBATCH --output=slurm_logs/m2m_ft_ca_de_1gpu_amd_adapter%j.out
+#SBATCH --error=slurm_logs/m2m_ft_ca_de_1gpu_amd_adapter%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --qos=debug 
+
+load_config_amd_0102() {
+	module load gcc/10.2.0 rocm/4.0.1 intel/2018.4 mkl/2018.4 python/3.7.4
+
+	export LD_LIBRARY_PATH=/gpfs/projects/bsc88/projects/bne/eval_amd/scripts_to_run/external-lib:$LD_LIBRARY_PATH
+
+	source /gpfs/projects/bsc88/projects/bne/eval_amd/ksenia/venv-fairseq/bin/activate
+
+	echo $SLURM_JOB_NODELIST
+}
+
+load_config_amd_0102
+
+python m2m_adapters/train.py
--- m2m_adapters/train.py
+++ m2m_adapters/train.py
@@ -6,8 +6,15 @@ from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2Se
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import AutoConfig, AutoModel
 import numpy as np
+import os
 
 import torch
+
+
+CHECKPOINT =  "facebook/m2m100_418M"
+DATASET_LOC = "data/ca-de"
+source_lang = "de"
+target_lang = "ca"
 
 def freeze_params(model):
     #Freeze all parameters not in the adapters
@@ -63,14 +70,11 @@ model = AutoModelForSeq2SeqLM.from_config(config)
 print("Model created", len(model.state_dict()))
 
 
-DATASET_LOC = "data/ca-de"
-source_lang = "de"
-target_lang = "ca"
 
 
 print("Loading tokenizer")
 
-tokenizer = AutoTokenizer.from_pretrained("facebook/m2m100_418M", 
+tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT, 
                                             return_tensors="pt", 
                                             src_lang=source_lang, 
                                             tgt_lang=target_lang)
@@ -82,7 +86,7 @@ tokenized_dataset = load_from_disk(DATASET_LOC)
 
 
 print("Loading pretrained")
-pretrained_state_dict = load_pretrained_state_dict('/home/bscuser/Documentos/m2m_adapters/model/m2m100_418M//pytorch_model.bin') # change
+pretrained_state_dict = load_pretrained_state_dict(os.path.join(CHECKPOINT, 'pytorch_model.bin')) # change
 print_mismatched_state_dict_keys(model.state_dict(),pretrained_state_dict)
 
 
@@ -93,18 +97,22 @@ model = freeze_params(model)
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model, padding=True)
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./results",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    save_total_limit=1,
-    num_train_epochs=1,
-    fp16=False,
-    save_strategy="epoch",
-    logging_strategy="epoch",
-)
+        output_dir="./results/parallel-adapter",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        weight_decay=0.01,
+        save_total_limit=1,
+        num_train_epochs=1,
+        fp16=False,
+        load_best_model_at_end=True,
+        save_strategy="steps",
+        save_steps=20000,
+        evaluation_strategy="steps",
+        eval_steps=10000,
+        logging_strategy="steps",
+     )
+
 
 trainer = Seq2SeqTrainer(
     model=model,
